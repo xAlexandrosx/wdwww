11035fd00a75f52cdeeb244722f64f7e11c965f2
Alexandros <olek4004@gmail.com>
2025-05-22 09:21:20 +0200

    topbar prototype

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 let currentLang = 'en';
 document.getElementById('langToggle')
     .addEventListener('click', () => {
-    currentLang = currentLang === 'en' ? 'pl' : 'en';
-    document.getElementById('langToggle')
-        .textContent = currentLang === 'en' ? 'PL' : 'EN';
+        currentLang = currentLang === 'en' ? 'pl' : 'en';
+        document.getElementById('langToggle')
+            .textContent = currentLang === 'en' ? 'EN' : 'PL';
+        document.getElementById('search')
+            .placeholder = currentLang === 'en' ? 'Search Products...' : 'Wyszukaj Produkty...';
+
     });
 
 // Dark mode toggle
 document.getElementById('darkModeToggle')
     .addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-});
+        document.body.classList.toggle('dark');
+
+        const isDark = document.body.classList.contains('dark');
+        document.getElementById('darkModeToggle').textContent = isDark ? '🌙' : '☀️';
+    });
+
